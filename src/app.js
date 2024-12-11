ca1d33bb4d33c5a0ee85a07a9d00f6f18b973d27
PavithraBusipalli <busipallipavitra@gmail.com>
2024-12-11 15:42:50 +0530

    wildcardErrorHandling

--- src/app.js
+++ src/app.js
@@ -1,40 +1,17 @@
 const express = require('express');
 
 const app = express();
-const {adminAuth, userAuth} = require('./middleware/auth');
 
-// app.use('/user', adminAuth);
-// app.use('/admin', userAuth)
-
-app.get('/user/getAllData', adminAuth, (req, res) => {
-    console.log('sent all data');
-    res.send('all data sent')
+app.get('/getUserData', (req, res, next) => {
+    throw new Error('something went wrong');
+    res.send('User data sending..!');
 });
 
-app.get('/user/updateData',adminAuth, (req, res) => {
-    console.log('updating the user');
-    res.send('update success');
-});
-
-app.get('/user/deleteUser', adminAuth, (req, res) => {
-    console.log('deleting the user');
-    res.send('deleting success')   
+app.use('/', (err, req, res, next) => {
+    if(err) {
+        // console.log(err);
+        res.status(500).send('Something went wrong!!');
+    }
 })
-
-app.get('/admin/getAllData', userAuth, (req, res) => {
-    console.log('sent all data');
-    res.send('all data sent')
-});
-
-app.get('/admin/updateData', userAuth, (req, res) => {
-    console.log('updating the user');
-    res.send('update success');
-});
-
-app.get('/admin/deleteUser', userAuth, (req, res) => {
-    console.log('deleting the user');
-    res.send('deleting success')   
-})
-
 
 app.listen(3000, () => console.log('Server is listening to the port: 3000..!'));
