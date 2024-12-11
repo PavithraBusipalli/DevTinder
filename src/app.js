0b7179f18f48053e4a9dec08a183e65085fbf5c4
PavithraBusipalli <busipallipavitra@gmail.com>
2024-12-11 14:58:17 +0530

    Request handlers

--- src/app.js
+++ src/app.js
@@ -3,11 +3,30 @@ const express = require('express');
 const app = express();
 
 // Request handlers
-app.use('/test', (req, res) => {
-    console.log(req.url.length);
-    res.send('Testing the handler  yes test')
-})
+// app.get('/user/:userId/:name/:password', (req, res) => {
+//     // console.log(req.query );
+//     console.log(req.params);
+//     res.send('Yesss')
+// })
 
+app.use('/user', (req, res, next) => {
+    console.log('Handling the route user!');
+    // res.send('1st response');
+    
+    // res.send('1st respo nse');
+    next();
+},
+    (req, res, next) => {
+        console.log('Handling the route user 2'); 
+        // res.send('2nd response')
+        next();
+    },
+    (req, res) => {
+        console.log('Handling the 3rd response!');
+        res.send('3r d response');
+        // next();
+    },
+);
 
 app.listen(2350, () => {
     console.log('server is running..!!');
