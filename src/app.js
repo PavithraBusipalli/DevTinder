dd56d140ec2c36089da219eae2d453928f32c4cb
PavithraBusipalli <busipallipavitra@gmail.com>
2024-12-12 12:24:19 +0530

    models methods

--- src/app.js
+++ src/app.js
@@ -3,24 +3,70 @@ const connectDB = require('./config/database');
 const { User } = require('./models/user');
 const app = express();
 
+// It will work of all methods to handle json data processing..!
+app.use(express.json());
 
-
+// Creating user
 app.post('/signup', async (req, res) => {
-    const userObj = {
-        firstName: "Sam",
-        lastName: "Reddy",
-        email: "Samreddy@gmail.com",
-        password: "sam@2024",
-        phoneNo: 9012345678,
-        gender: "Female"
-    }
-    //creating instance of user model
-    const user = new User(userObj);
+    const user = new User(req.body);
     try {
         await user.save();
-        res.send('User created successfully!')
+        res.send('User created successfully');
     } catch(err) {
-        res.status(400).json(err);
+        res.send('Error: ', err);
+    }
+})
+
+// Get user by email
+app.get('/user', async (req, res) => {
+    const userEmail = req.body.email;
+    try {
+        const user = await User.findOne({email: userEmail});
+        if(!user) {
+            res.status(404).send('User not found');
+        } else {
+            res.send(user);
+        }
+    } catch (err) {
+        res.status(400).send('Something went wrong');
+    }
+})
+
+
+// Get all the users 
+app.get('/getAllUsers', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch(err) {
+        res.status(400).send("Something went wrong..!!");
+    }
+})
+
+// Delete the user by id
+app.delete('/deleteUser', async (req, res) => {
+    const userId = req.body.id;
+    try {
+        const deltUser = await User.findByIdAndDelete(userId);
+        if(deltUser) {
+            res.send("User deleted successfully!");
+        } else {
+            res.send("User not found to delete")
+        }
+    } catch(err) {
+        res.status(400).send("Something went wrong");
+    } 
+})
+
+// Update the user by id
+app.put('/updateUser', async (req, res) => {
+    const userId = req.body.id;
+    const data = req.body;
+    try {
+        const user = await User.findByIdAndUpdate(userId, data);
+        res.send('User updated successfully');
+    } catch(err) {
+        res.status(400).send('Something went wrong');
     }
 })
 
